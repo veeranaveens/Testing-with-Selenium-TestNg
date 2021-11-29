5a9cdaf00f9a8df918f80a4dbd8471a44532cb0b
naveen-veera <veeranaveen.sethuraman@gmail.com>
2021-11-29 11:35:10 +0530

    testing-selenium

---
+++ src/main/java/ai/iamneo/testing/Testing_Selenium_TestNg/App.java
@@ -0,0 +1,13 @@
+package ai.iamneo.testing.Testing_Selenium_TestNg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/AppTest.java
@@ -0,0 +1,44 @@
+package ai.iamneo.testing.Testing_Selenium_TestNg;
+
+import org.testng.annotations.Test;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
+public class AppTest {
+
+	ChromeOptions chromeOptions = new ChromeOptions();
+	WebDriver driver = null;
+
+	@BeforeTest
+	public void beforeTest() throws Exception {
+		System.setProperty("webdriver.chrome.driver", "/home/coder/project/workspace/chromedriver");
+		driver = new RemoteWebDriver(new URL("http://172.17.0.3:4444"), chromeOptions);
+	}
+
+	@Test
+	public void TestCase_1() throws InterruptedException {
+		driver.get("https://admin.pscollege841.exam.ly/");
+		Thread.sleep(5000);  
+		String title = driver.getTitle();
+		Assert.assertEquals(title, "PS College");
+	}
+	@Test
+	public void TestCase_2() throws InterruptedException {
+		driver.get("https://admin.pscollege841.exam.ly/");
+		Thread.sleep(5000);  
+		String title = driver.getTitle();
+		Assert.assertEquals(title, "PS College");
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+	}
+
+}
